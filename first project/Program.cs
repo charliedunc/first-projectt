022d5dc893d399486d5a1fb4d82eac00b8ed92eb
Charlie Duncan <1charlie.duncan@gmail.com>
2025-02-05 20:01:50 -0500

    added new program address

--- first project/Program.cs
+++ first project/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template There are 3 programs in this project, each on their own branch
-Console.WriteLine("Hello, Worlddd!");
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    string name, address, phone;
+    name = "Sally Schaffner";
+    address = "101 My Street, Newport News, VA";
+    phone = "757-333-4444";
+    cout << name << end1;
+    cout << address << end1;
+    cout << phone << end1;
+    return 0;
+}
